d0fad436ce7fb55052218c9ee71209cb4b39d0d7
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2023-09-09 00:40:15 +0200

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,17 +1,45 @@
 from ursina import *
 from ursina.prefabs.dropdown_menu import DropdownMenu, DropdownMenuButton
+from ursina.prefabs.video_recorder import VideoRecorderUI
 
 app = Ursina()
+
+error = Audio(
+    'assets/sounds/error.mp3',
+    loop=False,
+    autoplay=False
+)
 
 
 class MenuButton(Button):
     def __init__(self, text='', **kwargs):
-        super().__init__(text, scale=(.25, .075), highlight_color=color.azure, font='assets/fonts/Audiowibe/Audiowibe.ttf', **kwargs)
-        self.alpha = 0
+        super().__init__(text, scale=(.25, .075), origin=(0, 0), highlight_color=color.azure,
+                         font='assets/fonts/Audiowibe/Audiowibe.ttf', **kwargs)
+        self.alpha = 1
         self.font = 'assets/fonts/Audiowibe/Audiowibe.ttf'
 
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    def on_mouse_enter(self):
+        if not self.disabled and self.model:
+            self.scale = (.28, .085)
+
+            if self.highlight_scale != 1:
+                self.scale = (.28, .085)
+
+        if hasattr(self, 'tooltip'):
+            self.tooltip.enabled = True
+
+    def on_mouse_exit(self):
+        if not self.disabled and self.model:
+            self.scale = (.25, .075)
+
+            if not mouse.left and self.highlight_scale != 1:
+                self.scale = (.25, .075)
+
+        if hasattr(self, 'tooltip'):
+            self.tooltip.enabled = False
 
 
 # button_size = (.25, .075)
@@ -22,38 +50,45 @@ load_menu = Entity(parent=menu_parent)
 options_menu = Entity(parent=menu_parent)
 
 state_handler = Animator({
-    'main_menu' : main_menu,
-    'load_menu' : load_menu,
-    'options_menu' : options_menu,
-    }
+    'main_menu': main_menu,
+    'load_menu': load_menu,
+    'options_menu': options_menu,
+}
 )
-
 
 # main menu content
 main_menu.buttons = [
-    MenuButton('start', icon='assets/textures/wood_button1', on_click=Func(setattr, state_handler, 'state', 'load_menu')),
-    MenuButton('options', icon='assets/textures/wood_button1', on_click=Func(setattr, state_handler, 'state', 'options_menu')),
-    MenuButton('quit', icon='assets/textures/wood_button1', on_click=Sequence(Wait(.01), Func(sys.exit))),
+    MenuButton('start', on_click=Func(setattr, state_handler, 'state', 'load_menu')),
+    MenuButton('options', on_click=Func(setattr, state_handler, 'state', 'options_menu')),
+    MenuButton('quit', on_click=Sequence(Wait(.01), Func(sys.exit))),
 ]
 for i, e in enumerate(main_menu.buttons):
     e.parent = main_menu
-    e.y = (-i-2) * button_spacing
+    e.y = (-i - 2) * button_spacing
 
 
 def start_game():
     menu_parent.enabled = False
 
 
+def start_multiplayer():
+    print_on_screen('Comming soon...')
+
+
 # load menu content
-MenuButton(parent=load_menu, icon='assets/textures/wood_button2', text=f'History', y=-1 * button_spacing, on_click=start_game)
-MenuButton(parent=load_menu, icon='assets/textures/wood_button2', text=f'Dead Game', y=-2 * button_spacing, on_click=start_game)
-MenuButton(parent=load_menu, icon='assets/textures/wood_button2', text=f'Multiplayer', y=-3 * button_spacing, on_click=start_game)
+MenuButton(parent=load_menu, text=f'History', y=-1 * button_spacing,
+           on_click=start_game)
+MenuButton(parent=load_menu, text=f'Dead Game', y=-2 * button_spacing,
+           on_click=start_game)
+MenuButton(parent=load_menu, text=f'Multiplayer', y=-3 * button_spacing,
+           on_click=start_multiplayer)
 
-load_menu.back_button = MenuButton(parent=load_menu, icon='assets/textures/wood_button3', text='back', y=((-i-2) * button_spacing), on_click=Func(setattr, state_handler, 'state', 'main_menu'))
-
+load_menu.back_button = MenuButton(parent=load_menu, text='back',
+                                   y=((-i - 2) * button_spacing),
+                                   on_click=Func(setattr, state_handler, 'state', 'main_menu'))
 
 # options menu content
-review_text = Text(parent=options_menu, x=.275, y=.25, text='Preview text', origin=(-.5,0))
+review_text = Text(parent=options_menu, x=.275, y=.25, text='Preview text', origin=(-.5, 0))
 for t in [e for e in scene.entities if isinstance(e, Text)]:
     t.original_scale = t.scale
 
@@ -66,25 +101,44 @@ def set_text_scale():
 
 
 text_scale_slider.on_value_changed = set_text_scale
-volume_slider = Slider(0, 1, default=Audio.volume_multiplier, step=.1, text='Master Volume:', parent=options_menu, x=-.25)
+volume_slider = Slider(0, 1, default=Audio.volume_multiplier, step=.1, text='Master Volume:', parent=options_menu,
+                       x=-.25)
+
+bg_music = Audio(
+    'assets/sounds/music3.mp3',
+    loop=True,
+    autoplay=True,
+    volume=1
+)
 
 
 def set_volume_multiplier():
     Audio.volume_multiplier = volume_slider.value
+    bg_music.volume = volume_slider.value
 
 
 volume_slider.on_value_changed = set_volume_multiplier
 
+
 def low():
-    window.size = (720, 480)
+    if not window.size == (720, 480):
+        window.size = (720, 480)
+    else:
+        print_on_screen("The screen size are already low", scale=2)
 
 
 def medium():
-    window.size = (1280, 720)
+    if not window.size == (1280, 720):
+        window.size = (1280, 720)
+    else:
+        print_on_screen("The screen size are already medium", scale=2)
 
 
 def high():
-    window.size = (1920, 1080)
+    if not window.size == (1920, 1080):
+        window.size = (1920, 1080)
+    else:
+        print_on_screen("The screen size are already high", scale=2)
 
 
 def fullscreen():
@@ -119,11 +173,11 @@ win.y = -.2
 reso.parent = options_menu
 win.parent = options_menu
 
-options_back = MenuButton(parent=options_menu, icon='assets/textures/wood_button3', text='Back', x=-.25, origin_y=3, origin_x=-.5, on_click=Func(setattr, state_handler, 'state', 'main_menu'))
+options_back = MenuButton(parent=options_menu, text='Back', x=-.25, origin_y=3,
+                          origin_x=-.5, on_click=Func(setattr, state_handler, 'state', 'main_menu'))
 
 for i, e in enumerate((text_scale_slider, volume_slider, options_back)):
     e.y = -i * button_spacing
-
 
 # animate the buttons in nicely when changing menu
 for menu in (main_menu, load_menu, options_menu):
@@ -131,19 +185,19 @@ for menu in (main_menu, load_menu, options_menu):
         for i, e in enumerate(menu.children):
             e.original_x = e.x
             e.x += .1
-            e.animate_x(e.original_x, delay=i*.05, duration=.1, curve=curve.out_quad)
+            e.animate_x(e.original_x, delay=i * .05, duration=.1, curve=curve.out_quad)
 
-            e.alpha = 0
-            e.animate('alpha', 0, delay=i*.05, duration=.1, curve=curve.out_quad)
+            e.alpha = 0.8
+            e.animate('alpha', 0.8, delay=i * .05, duration=.1, curve=curve.out_quad)
 
             if hasattr(e, 'text_entity'):
-                e.text_entity.alpha = 0
-                e.text_entity.animate('alpha', 1, delay=i*.05, duration=.1)
+                e.text_entity.alpha = 0.8
+                e.text_entity.animate('alpha', 1, delay=i * .05, duration=.1)
+
 
     menu.on_enable = animate_in_menu
 
-
-background = Entity(parent=menu_parent, model='quad', texture='shore', scale=(camera.aspect_ratio,1), color=color.white, z=1, world_y=0)
-
+background = Entity(parent=menu_parent, model='quad', texture='shore', scale=(camera.aspect_ratio, 1),
+                    color=color.white, z=1, world_y=0)
 
 app.run()
